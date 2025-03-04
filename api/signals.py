f737d51d67b3fff50457473a5793515b2765ac17
SoorajSubair <soorajms2111999@gmail.com>
2025-03-04 20:39:38 +0530

    bug fix

--- api/signals.py
+++ api/signals.py
@@ -14,6 +14,7 @@ def call_create_task(sender, instance, **kwargs):
 @receiver(post_save, sender=Task)
 def task_completion_signal(sender, instance, **kwargs):
     # Check if the task name matches and is marked as completed
+    print(instance.name)
     if instance.name == "Digital copies sent to client" and instance.completed:
         # Perform your action here (e.g., send notification, log entry, etc.)
         add_archieved_tag_to_ghl(instance.contact.location_id, instance.contact.contact_id)
